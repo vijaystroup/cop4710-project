f6d3f3335387bfc7d9842d04e0cc2a1dcb1d4ee5
Vijay Stroup <vijaystroup@gmail.com>
2022-12-03 22:53:59 -0500

    survey Card

--- components/surveyCards.tsx
+++ components/surveyCards.tsx
@@ -1,21 +1,18 @@
 import { type FC } from 'react'
+import Link from 'next/link'
 
 interface surveyCardInterface {
-    surveyName:String,
-    surveyDesc:String,
-    responseCount:Number
+    surveyName: string
+    surveyDesc: string
+    id: number
 }
 
-const SurveyCards = ({surveyName, surveyDesc, responseCount}: surveyCardInterface) => {
+const SurveyCards: FC = ({surveyName, surveyDesc, id}: surveyCardInterface) => {
     return(
-        <a href="#" className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-md hover:bg-gray-100 dark:bg-gray-600 dark:border-gray-700 dark:hover:bg-gray-700">
+        <Link href={`/survey/${id}`} className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-md hover:bg-gray-100 dark:bg-gray-600 dark:border-gray-700 dark:hover:bg-gray-700">
             <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{surveyName}</h5>
             <p className="font-normal text-gray-600 dark:text-gray-400">{surveyDesc}</p>
-            <div>
-                <text className='font-normal text-gray-600 dark:text-gray-200'>Response Count: </text>
-                <text className='font-normal text-gray-600 dark:text-gray-200'>{responseCount.toString()}</text>
-            </div>
-        </a>
+        </Link>
     )
 }
 
