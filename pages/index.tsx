3a5fca97a5c660b88c9a329c6ee3ae532a429bd2
Vijay Stroup <vijaystroup@gmail.com>
2022-12-05 16:03:35 -0500

    fetch user surveys at login

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,34 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-// import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import Header from '../components/header'
 import SurveyCards from '../components/surveyCards'
-// import user from '../lib/user'
-
-
+import user from '../lib/user'
 
 const Home: NextPage = () => {
-  
-  // useEffect(() => {
-  //   console.log('hi')
-  //   console.log(user)
-  // }, [])
+  const [surveys, setSurveys] = useState<{
+    id: number
+    title: string
+    description: string
+    start: string
+    end: string
+  }[]>([])
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch('/api/getUserSurveys', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ user_id: user.id })
+      })
+      const data = await res.json()
+      return data
+    }
+
+    fetchData().then(data => setSurveys(data.data))
+  }, [])
 
   return (
     <>
@@ -22,9 +38,25 @@ const Home: NextPage = () => {
 
       <Header />
 
-      <main className='h-[calc(100vh-80px-40px)] bg-gray-900'>
-        <h1 className='p-4 mb-4 text-5xl font-bold text-center text-red-600'>Home</h1>
-        <SurveyCards id={5} surveyDesc="tuikgfh" surveyName='dfghb' start='2022-1-12' end="2022-12-29"/>
+      <main className='h-[calc(100vh-80px-40px)] bg-gray-900 px-10'>
+        {!user.email && <h1 className='p-4 mb-4 text-5xl font-bold text-center text-red-600'>Sign in to get started!</h1>}
+        {user.email && <h1 className='p-4 mb-4 text-5xl font-bold text-center text-red-600'>Welcome, {user.email.split('@')[0]}!</h1>}
+        {surveys && 
+          <div className='flex flex-wrap justify-center gap-4'>
+            {surveys.map(survey => {
+              return (
+                <SurveyCards
+                  key={survey.id}
+                  surveyName={survey.title}
+                  surveyDesc={survey.description}
+                  id={survey.id}
+                  start={survey.start}
+                  end={survey.end}
+                />
+              )
+            })}
+          </div>
+        }
       </main>
     </>
   )
