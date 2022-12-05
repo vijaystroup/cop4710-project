1bf76ecf01341e952e392a4e43234ee1e369189c
Adam <leiadam74@gmail.com>
2022-12-05 15:00:51 -0500

    Changed the survey card look

--- components/surveyCards.tsx
+++ components/surveyCards.tsx
@@ -5,13 +5,28 @@ interface surveyCardInterface {
     surveyName: string
     surveyDesc: string
     id: number
+    start: string
+    end:string
 }
 
-const SurveyCards: FC = ({surveyName, surveyDesc, id}: surveyCardInterface) => {
+const SurveyCards: FC <surveyCardInterface> = ({surveyName, surveyDesc, id, start, end} ) => {
+    
+    const startDate = new Date(start)
+    const endDate = new Date(end)
+    const currentDate = new Date(new Date().toISOString().slice(0, 19).replace('T', ' '))
+    const active = startDate <= currentDate && currentDate <= endDate
     return(
         <Link href={`/survey/${id}`} className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-md hover:bg-gray-100 dark:bg-gray-600 dark:border-gray-700 dark:hover:bg-gray-700">
-            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{surveyName}</h5>
+            <div className="flex items-center justify-between">
+                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{surveyName}</h5>
+                <p className={`font-normal text-gray-600 dark:text-white rounded px-2 ${active ? 'bg-green-500/30' : 'bg-red-500/30'}`}>{active ? 'Status: Active' : 'Status: Finished'}</p>
+            
+            </div>
             <p className="font-normal text-gray-600 dark:text-gray-400">{surveyDesc}</p>
+            <div className="flex items-center justify-between">
+                <p className="font-normal text-gray-600 dark:text-white">{start}</p>
+                <p className="font-normal text-gray-600 dark:text-white">{end}</p>
+            </div>
         </Link>
     )
 }
