81715fc499721acaa2c2377f6ff24fa1c8c0038a
Vijay Stroup <vijaystroup@gmail.com>
2022-12-05 16:28:18 -0500

    email on signup

--- pages/api/register.ts
+++ pages/api/register.ts
@@ -1,4 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
+import nodemailer from 'nodemailer'
 import db from '../../db/client'
 
 type Data = {
@@ -16,7 +17,7 @@ export default function handler(
     INSERT INTO user (email, password)\
     VALUES (?, ?)\
   `, [email, password],
-    (error, results, fields) => {
+    async (error, results, fields) => {
       if (error) {
         if (error.code === 'ER_DUP_ENTRY') {
           res.status(400).json({ email, status: 'error' })
@@ -24,6 +25,23 @@ export default function handler(
           res.status(500).json({ email, status: 'error' })
         }
       } else {
+        const transporter = nodemailer.createTransport({
+          service: 'gmail',
+          auth: {
+            user: process.env.EMAIL_USER,
+            pass: process.env.EMAIL_PASS,
+          }
+        })
+
+        const info = await transporter.sendMail({
+          from: process.env.EMAIL_USER,
+          to: email, // list of receivers
+          subject: 'Survey Signup', // Subject line
+          text: 'Thank you for signing up to 📋 Survey!', // plain text body
+        })
+
+        console.log('Message sent: %s', info.messageId)
+
         res.status(200).json({ email, status: 'success' })
       }
     }
